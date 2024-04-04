aa3e1301b05c1aa13d661927e354c9570ed42eff
yeang34gsd <128847206+yeang34gsd@users.noreply.github.com>
2024-04-03 21:52:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             downloadIcon.addEventListener('click', function() {
 
-                abrirVistaPrevia(image.imageUrl);
+                window.open(image.imageUrl, '_blank');
 
             });
 
@@ -128,8 +128,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Mostramos la galería de la categoría seleccionada
 
-        const galeria = document.getElementById("grid-container");
-        galeria.style.display = "block"; // Mostrar la galería principal
+        document.getElementById("galeria-" + categoria).style.display = "block";
+
     }
 
     // Obtener todos los elementos con la clase "download-icon"
@@ -160,3 +160,96 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 });
+
+
+const imagenesPorCategoria = {
+
+    "chibi": [
+
+        { url: "https://i.pinimg.com/236x/8e/f6/ab/8ef6ab8be1b190d489477a64178b7f7c.jpg", descripcion: "Descripción de la imagen 1" },
+
+        { url: "imagen2.jpg", descripcion: "Descripción de la imagen 2" },
+
+    ],
+
+    
+};
+
+function mostrarCategoria(categoria) {
+
+    const galeria = document.getElementById("gallery");
+
+    galeria.innerHTML = "";
+
+    const imagenes = imagenesPorCategoria[categoria];
+
+    if (imagenes) {
+
+        shuffle(imagenes);
+
+        
+
+        imagenes.forEach(function(imagen) {
+
+            const gridItem = document.createElement("div");
+
+            gridItem.className = "grid-item";
+
+            const img = document.createElement("img");
+
+            img.src = imagen.url;
+
+            img.alt = categoria;
+
+            const descripcion = document.createElement("div");
+
+            descripcion.className = "description";
+
+            descripcion.textContent = imagen.descripcion;
+
+            const downloadIcon = document.createElement("div");
+
+            downloadIcon.className = "download-icon";
+
+            downloadIcon.innerHTML = "&#x2B07;";
+
+            downloadIcon.addEventListener('click', function() {
+
+                window.open(imagen.url, '_blank');
+
+            });
+            gridItem.appendChild(img);
+
+            gridItem.appendChild(descripcion);
+
+            gridItem.appendChild(downloadIcon);
+
+            galeria.appendChild(gridItem);
+
+        });
+
+    }
+
+}
+
+function shuffle(array) {
+
+    for (let i = array.length - 1; i > 0; i--) {
+
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]];
+
+    }
+
+}
+
+window.onload = function() {
+
+    const categorias = Object.keys(imagenesPorCategoria);
+
+    const categoriaAleatoria = categorias[Math.floor(Math.random() * categorias.length)];
+
+    mostrarCategoria(categoriaAleatoria);
+
+};
